a9a9f2c6b16f37ab451582feaaa2288088d8ea2e
romekk21 <romek.kozdrowicz@gmail.com>
2023-03-20 16:38:54 -0400

    Added login and logout

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/accounts/login/')
 # Create your views here.
 def logIn_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Login page</h1>")
+    return HttpResponse("Hello")
+#return render(request, "registration/login.html", {})
 
 def instructorHome_view(request, *args, **kwargs):
     return render(request, "instructorHome.html", {})
