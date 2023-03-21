899814f268527d2e3a743891a0119e6e7f6a9e78
anniemerritt <merritan@bc.edu>
2023-03-21 14:00:52 -0400

    Adding custom user model

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN",'admin'
+        STUDENT = "STUDENT",'student'
+        INSTRUCTOR = "INSTRUCTOR", 'instructor'
+
+    base_role = Role.STUDENT
+
+    role = models.CharField(max_length=50, choices=Role.choices)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **kwargs)
+        
