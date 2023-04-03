5af1714ba737fc5a2ca3c0685b9688f071fb82ad
CRUVOD <haoran0088@hotmail.com>
2023-04-03 15:13:56 -0400

    Changed user editability
    Minor change, now they can be changed in django admin

--- src/application/models.py
+++ src/application/models.py
@@ -11,7 +11,7 @@ class Application(models.Model):
     interest = models.TextField(blank=True, null=True)
     experience = models.TextField(blank=True, null=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, blank=True, null=True)
-    applicantUser = models.ForeignKey(User, on_delete=models.CASCADE, editable = False)
+    applicantUser = models.ForeignKey(User, on_delete=models.CASCADE, editable = True, blank=True, null=True)
 
     def __str__(self):
         return str(self.course.courseNumber) + "/" + str(self.course.courseSection) + "-" + self.name
