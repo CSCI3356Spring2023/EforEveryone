8fb3965a944e771db3e3bc99ce8bf9478db072ef
anniemerritt <merritan@bc.edu>
2023-05-11 19:02:29 -0400

    cohort and relation to cs department now part of application

--- src/application/forms.py
+++ src/application/forms.py
@@ -4,6 +4,8 @@ from .models import Application
 class ApplicationCreationForm(forms.ModelForm):
     email = forms.CharField(label='Please enter your bc.edu email')
     eagleID = forms.IntegerField(label='Please enter your 8-digit Eagle ID')
+    cohort = forms.ChoiceField(choices=Application.COHORT_CHOICES, label='Select your class year')
+    relation = forms.ChoiceField(choices=Application.RELATION_CHOICES, label='Select your relation to the CS department')
     courseHistory = forms.CharField(label='Have you taken this course and if so what grade did you receive?')
     interest = forms.CharField(label='Why are you interested in being a TA for this course?')
     experience = forms.CharField(label='Please explain any relevant experience you have with course material')
@@ -13,6 +15,8 @@ class ApplicationCreationForm(forms.ModelForm):
         fields = [
             'email',
             'eagleID',
+            'cohort',
+            'relation',
             'courseHistory',
             'interest',
             'experience',
--- src/application/models.py
+++ src/application/models.py
@@ -16,6 +16,20 @@ class Application(models.Model):
     applicantUser = models.ForeignKey(User, on_delete=models.CASCADE, editable = True, blank=True, null=True)
     pendingInstructorAccept = models.BooleanField(default=True)
     acceptedByStudent = models.BooleanField(default=False)
+    COHORT_CHOICES = [
+        ('Freshman', 'Freshman'),
+        ('Sophomore', 'Sophomore'),
+        ('Junior', 'Junior'),
+        ('Senior', 'Senior'),
+    ]
+    cohort = models.CharField(max_length=20, choices=COHORT_CHOICES, blank=True, null=True)
+    RELATION_CHOICES = [
+        ('Major BS', 'Major BS'),
+        ('Major BA', 'Major BA'),
+        ('Minor', 'Minor'),
+        ('Other', 'Other'),
+    ]
+    relation = models.CharField(max_length=20, choices=RELATION_CHOICES, blank=True, null=True)
 
     def __str__(self):
         return str(self.course.courseNumber) + "/" + str(self.course.courseSection) + "-" + self.name
